d96d42bd84d46afeb3d44e903ef38beb75b22a55
Nalivayko Polina <71880900+Nalivayko13@users.noreply.github.com>
2021-05-31 21:01:50 +0300

    fead: add task

--- app/src/main/java/com/rsschool/android2021/MainActivity.java
+++ app/src/main/java/com/rsschool/android2021/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentListner {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,11 +19,22 @@ public class MainActivity extends AppCompatActivity {
     private void openFirstFragment(int previousNumber) {
         final Fragment firstFragment = FirstFragment.newInstance(previousNumber);
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, firstFragment);
-        // TODO: invoke function which apply changes of the transaction
+            transaction.replace(R.id.container, firstFragment);
     }
 
     private void openSecondFragment(int min, int max) {
-        // TODO: implement it
+        final Fragment secondFragment = SecondFragment.newInstance(min,max);
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.container, secondFragment);
     }
+    @Override
+    public void actionPerformed2(int min, int max) {
+        openSecondFragment(min, max);
+    }
+    @Override
+    public void actionPerformed1(int number) {
+        openFirstFragment(number);
+    }
+
 }
+
